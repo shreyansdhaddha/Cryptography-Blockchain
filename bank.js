e30d383ec82f92a92943c465cef11d541cd430e2
Shreyans Dhaddha <shreyansdha_ddha@hotmail.com>
2018-11-05 00:00:30 -0800

    Supporting transaction history log file

--- bank.js
+++ bank.js
@@ -1,16 +1,50 @@
 var jsonStream = require('duplex-json-stream')
 var net = require('net')
+var fs = require('fs')
+
 
 var balance = 0;
 var log = [];
+const logFile = 'transactionlog.txt';
+var isLogLoaded = false;
+
+function add(total, amount) { return total+= amount; }
+function subtract(total, amount) { return total-= amount; }
 
 function getBalance() {
     return balance;
 }
 
+function updateLogToFile() {
+    fs.writeFile(logFile, JSON.stringify(log), function(err) {
+        if(err)
+            throw err;
+    });
+}
+
+function updateLogFromFile() {
+    console.log("**** Reading previous Transaction File ****");
+    fs.stat(logFile, function(err) {
+        if(err == null) {
+            fs.readFile(logFile, "utf8", function(err, data) {
+                var objects = JSON.parse(data);
+
+                //Add the objects inside the interal log
+                for(i=0; i<objects.length; i++) {
+                    doCore(objects[i]);
+                }
+                printLog("LOADED");
+            });
+        }
+    });
+
+    isLogLoaded = true;
+    return true;
+}
+
 function addLogEntry (command, amount) {
     log.push({cmd: command, amount: amount});
-    return true;
+    updateLogToFile();
 }
 
 function setDeposit(amount) {
@@ -34,8 +68,17 @@ function getBalanceWithCommand() {
 }
 
 function doCore(json) {
-    if('cmd'in json)
+    if('cmd' in json)
         return doCommand(json);
+}
+
+//Logging
+function printLog(externalMsg) {
+    console.log("****", externalMsg == null ? "" : externalMsg, "TRANSACTION LOG ****")
+    for(i=0; i<log.length; i++) {
+        console.log(log[i]);
+    }
+    console.log("****", externalMsg == null ? "" : externalMsg, "TRANSACTION LOG ****")
 }
 
 function doCommand(obj) {
@@ -53,17 +96,12 @@ function doCommand(obj) {
     }
 }
 
-//Logging
-function printLog() {
-    console.log("**** TRANSACTION LOG ****")
-    for(i=0; i<log.length; i++) {
-        console.log(log[i]);
-    }
-    console.log("**** TRANSACTION LOG ****")
-}
-
 var server = net.createServer(function (socket) {
     socket = jsonStream(socket)
+
+    if(!isLogLoaded)
+        updateLogFromFile();
+
     socket.on('data', function(data) {
 
         console.log("Bank received: ", data)
