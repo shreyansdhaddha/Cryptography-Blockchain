c85727d71c31800969dc696417e83ae52488d9b4
Shreyans Dhaddha <shreyansdha_ddha@hotmail.com>
2018-11-04 20:20:30 -0800

    Implementation of Deposit and Withdraw

--- bank.js
+++ bank.js
@@ -1,23 +1,76 @@
 var jsonStream = require('duplex-json-stream')
 var net = require('net')
 
-function doCore(json) {
-    if(json["cmd"])
-        return doCommand(json["cmd"]);
+var balance = 0;
+var log = [];
+
+function getBalance() {
+    return balance;
 }
 
-function doCommand(jsonCommand) {
-    switch(jsonCommand) {
-        case 'balance':
-            return {cmd: 'balance', balance: 0};
+function addLogEntry (command, amount) {
+    log.push({cmd: command, amount: amount});
+    return true;
+}
+
+function setDeposit(amount) {
+    if(amount > 0)
+    {
+        balance += amount;
+        addLogEntry('deposit', amount);
     }
+}
+
+function setWithdraw(amount) {
+    if(amount > 0 && (balance-amount) >= 0)
+    {
+        balance -= amount;
+        addLogEntry('withdraw', amount);
+    }
+}
+
+function getBalanceWithCommand() {
+    return {cmd: 'balance', balance: getBalance()};
+}
+
+function doCore(json) {
+    if('cmd'in json)
+        return doCommand(json);
+}
+
+function doCommand(obj) {
+    switch(obj['cmd']) {
+        case 'balance':
+            return getBalanceWithCommand();
+        case 'deposit':
+            setDeposit(obj['amount']);
+            return getBalanceWithCommand();
+        case 'withdraw':
+            setWithdraw(obj['amount']);
+            return getBalanceWithCommand();
+        default:
+            return "Error - Command Not Found";
+    }
+}
+
+//Logging
+function printLog() {
+    console.log("**** TRANSACTION LOG ****")
+    for(i=0; i<log.length; i++) {
+        console.log(log[i]);
+    }
+    console.log("**** TRANSACTION LOG ****")
 }
 
 var server = net.createServer(function (socket) {
     socket = jsonStream(socket)
     socket.on('data', function(data) {
+
         console.log("Bank received: ", data)
-        socket.write(doCore(data))
+
+        socket.write(doCore(data));
+        
+        printLog();
     })
 })
 server.listen(3876)
--- teller.js
+++ teller.js
@@ -1,12 +1,61 @@
 var jsonStream = require('duplex-json-stream')
 var net = require('net')
 
-var client = jsonStream(net.connect(3876))
+var depositAmount = 0;
+var arguments = [];
 
-client.write({cmd: 'balance'})
-client.on('data', function (data) {
-    console.log("Teller received: ", data)
-    client.destroy();
-})
+run();
 
-client.end
+
+function run()
+{   
+    //Store the argument
+    arguments = process.argv.slice();
+
+    if(arguments.length <= 2 || arguments.length > 4) {
+        console.log("Invalid number of arguments: Found", arguments.length);
+    }
+    else {
+        sendCommand(arguments[2], Number(arguments[3]));
+    }
+}
+
+function sendCommand(accountCommand, value) {
+
+    var client = jsonStream(net.connect(3876))
+
+    if(accountCommand == 'balance') {
+        client.write({cmd: accountCommand});
+        client.on('data', function (data) {
+            console.log("Teller received: ", data)
+            client.destroy();
+        })
+    }
+    else if(accountCommand == 'deposit') {
+        client.write({cmd: accountCommand, amount:value})
+
+        client.on('data', function (data) {
+            console.log("Teller depositing: ", value);
+            console.log("Balance: ", data)
+            client.destroy();
+        })
+    }
+    else if(accountCommand == 'withdraw') {
+        client.write({cmd: accountCommand, amount:value})
+
+        client.on('data', function (data) {
+            console.log("Teller withdrawing: ", value);
+            console.log("Balance: ", data)
+            client.destroy();
+        })
+    }
+    else {
+        client.write({cmd: accountCommand})
+
+        client.on('data', function (data) {
+            console.log("Bank Response: ", data);
+            client.destroy();
+        })
+    }    
+    client.end
+}
